bc7c1ba743f1677ac2469a90a76d3c8e04626a62
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-16 10:53:24 +0800

    upload

---
+++ unicloud-day-by-day/pages/upload/upload.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<uni-file-picker v-model="imageValue" fileMediatype="image" mode="grid" @select="select" @progress="progress"
+			@success="success" @fail="fail" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imageValue: []
+			};
+		},
+		methods: {
+			// 获取上传状态
+			select(e) {
+				console.log('选择文件：', e)
+			},
+			// 获取上传进度
+			progress(e) {
+				console.log('上传进度：', e)
+			},
+
+			// 上传成功
+			success(e) {
+				console.log('上传成功')
+			},
+
+			// 上传失败
+			fail(e) {
+				console.log('上传失败：', e)
+			}
+		}
+
+	}
+</script>
+
+<style lang="scss">
+
+</style>
