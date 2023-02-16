3bca71e239cb57b2c37fa4002680e90bcfab2361
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-16 10:13:47 +0800

    edit

---
+++ unicloud-day-by-day/pages/feedback/feedback.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="u-content">
+		<!-- <u-parse :content="content"></u-parse> -->
+		<jinEdit class='st_edit' placeholder="请输入内容" @editOk="editOk" uploadFileUrl="/#" :height="'calc(100vh - 88rpx)'"></jinEdit> 
+	</view>
+</template>
+
+<script>
+	import jinEdit from '@/components/jin-edit/jin-edit.vue';
+	export default {
+		components: {
+			jinEdit
+		},
+		data() {
+			return {
+				content: `<p>露从今夜白，月是故乡明</p>
+				<img src="https://cdn.uviewui.com/uview/swiper/2.jpg" />
+				`
+			};
+		},
+		methods: {
+			// 点击发布
+			editOk(res) {
+				console.log(res);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.u-content {
+		// padding: 24rpx;
+		// height: calc(100vh - 50rpx);
+	}
+	
+</style>
